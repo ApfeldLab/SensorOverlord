ca2009509a62f90f701e5ca0cee6e42346981b88
Julian A. Stanley <stanley.ju@husky.neu.edu>
2019-03-27 00:10:30 -0400

    Update

--- R/Sensor_Methods.R
+++ R/Sensor_Methods.R
@@ -10,9 +10,6 @@
 #'
 #' @export
 #' @docType methods
-#' @rdname getR-generic
-#'
-
 setGeneric('getR', def = function(object, ...) standardGeneric("getR"))
 
 #' Method to get an array of R values from a sensor
@@ -48,7 +45,6 @@ setMethod('getR', "Sensor", definition =
 #' @return A generic function for getFractionMax
 #'
 #' @export
-#' @rdname getFractionMax-generics
 setGeneric('getFractionMax', def = function(object, ...)
     standardGeneric("getFractionMax"))
 
@@ -90,7 +86,6 @@ setMethod("getFractionMax", "Sensor", definition =
 #'
 #' @export
 #' @docType methods
-#' @rdname getE-generic
 setGeneric('getE', def = function(object, ...) standardGeneric("getE"))
 
 #' Get the redox potential (E) for a redox sensor
@@ -99,7 +94,8 @@ setGeneric('getE', def = function(object, ...) standardGeneric("getE"))
 #' redox potential corresponding to a given ratio (R) value
 #'
 #' @param object A redoxSensor object
-#' @param R A numeric value (can be an array) of ratio values
+#' @param R (Optional, defaults to getR(Object)
+#' A numeric value (can be an array) of ratio values
 #' @param temp The temperature, in Kelvin. Default is 295.15
 #'
 #' @return A numeric array of E values
@@ -109,13 +105,55 @@ setGeneric('getE', def = function(object, ...) standardGeneric("getE"))
 #' @rdname getE-redoxSensor
 setMethod("getE", "redoxSensor", definition =
               function(object, R, temp = 295.15) {
-                  return(
+
+                  # Get the object's R, if no R is passed
+                  if(missing(R)) {
+                      R = getR(object)
+                  }
                       return(object@e0 - (8.315 * temp)/(2 * 96.48104) *
                                  log(
                                      (object@delta *
                                           object@Rmax - object@delta * R) /
                                          (R - object@Rmin)))
-                  )
+              })
+
+#' Get the the derivative of the redox potential (dE/dR) (generic)
+#'
+#' @param object An object
+#' @param ... ...
+#'
+#' @return A numeric array of dE/dR values
+#'
+#' @export
+#' @docType methods
+setGeneric('getE_deriv', def = function(object, ...)
+    standardGeneric("getE_deriv"))
+
+#' Get the derivative of the redox potential (dE/dR) for a redox sensor
+#'
+#' For a given redox sensor at a certain temperature, returns the
+#' derivative of the redox potential corresponding to a given ratio (R) value
+#'
+#' @param object A redoxSensor object
+#' @param R (Optional, defaults to getR(Object)
+#' A numeric value (can be an array) of ratio values
+#'
+#' @return A numeric array of dE/dR values
+#'
+#' @export
+#' @docType methods
+#' @rdname getE_deriv-redoxSensor
+setMethod("getE_deriv", "redoxSensor", definition =
+              function(object, R) {
+
+                  # Get the object's R, if no R is passed
+                  if(missing(R)) {
+                      R = getR(object)
+                  }
+                  return(
+                      (-12.71*(object@Rmax - object@Rmin))/
+                          ((R - object@Rmin) * (R - object@Rmax))
+                         )
               })
 
 #' Get the error (generic)
@@ -127,8 +165,7 @@ setMethod("getE", "redoxSensor", definition =
 #'
 #' @export
 #' @docType methods
-#' @rdname getError-generic
-setGeneric('getError', def = function(object, ...) standardGeneric("getError"))
+setGeneric('getAbsError', def = function(object, ...) standardGeneric("getAbsError"))
 
 #' Get the error for a given sensor object
 #'
@@ -146,8 +183,8 @@ setGeneric('getError', def = function(object, ...) standardGeneric("getError"))
 #'
 #' @export
 #' @docType methods
-#' @rdname getError-sensor
-setMethod("getError", "Sensor", definition =
+#' @rdname getAbsError-sensor
+setMethod("getAbsError", "Sensor", definition =
               function(object, R, FUN, Error_Model, ...) {
                   answer <- c()
                   for (R_individual in R) {
@@ -171,12 +208,98 @@ setMethod("getError", "Sensor", definition =
                       # Append the maximum error to the answer
                       answer <- c(answer, max_error)
 
-                      # Print stuff
-                      print(paste(R_individual, R_lower, R_upper, value_error_up, value_error_down, max_error))
                   }
 
                   return(answer)
               })
+
+#' Get a table of the errors (generic)
+#'
+#' @param object An object
+#' @param ... ...
+#'
+#' @return A dataframe of errors
+#'
+#' @export
+#' @docType methods
+setGeneric('getErrorTable', def = function(object, ...)
+    standardGeneric("getErrorTable"))
+
+#' Get the error table for a given sensor object
+#'
+#' Each R will have a certain error, defined by the Error_Model parameter
+#' That error in R will propagate when applied to the FUN function parameter
+#' This method returns (1) The input Rs (2) the error in Rs
+#' (3) the results of the FUN function
+#' parameter (4) the upper bound of the FUN function (5) the lower bound
+#' of te fun function (6) the max absolute error for the FUN function
+#'
+#' @param object A sensor object
+#' @param R An array of numeric ratio values
+#' @param FUN A function that will be applied to R
+#' @param Error_Model A function in the form Error_Model(R) --> Error in R
+#' @param ... Extra parameters applied to the FUN function
+#'
+#' @return A dataframe of errors
+#'
+#' @export
+#' @docType methods
+#' @rdname getErrorTable-sensor
+setMethod("getErrorTable", "Sensor", definition =
+              function(object, R, FUN, Error_Model, ...) {
+                  max_error_acc <- c()
+                  lower_FUN_acc <-
+                  lower_error_acc <- c()
+                  upper_FUN_acc <- c()
+                  upper_error_acc <- c()
+                  R_error_acc <- c()
+                  FUN_acc <- c()
+
+                  for (R_individual in R) {
+                      R_error <- Error_Model(R_individual)
+                      R_error_acc <- c(R_error_acc, R_error)
+
+                      R_lower <- R_individual + R_error
+                      R_upper <- R_individual - R_error
+
+                      # Get & appendthe result of the function
+                      # applied to the R "actual"
+                      FUN_true <- FUN(object, (R_individual), ...)
+                      FUN_acc <- c(FUN_acc, FUN_true)
+
+
+                      # Get & append the absolute difference in running the function between R and the R + error
+                      # You may get an error, in which case the error is infinite
+                      upper_FUN <- suppressWarnings(FUN(object, R_upper, ...))
+                      upper_FUN <- ifelse(test = is.na(upper_FUN),
+                                          yes = Inf, no = upper_FUN)
+                      upper_error <- abs(upper_FUN - FUN_true)
+
+                      upper_FUN_acc <- c(upper_FUN_acc, upper_FUN)
+                      upper_error_acc <- c(upper_error_acc, upper_error)
+
+                      # Same thing, but with function between R and R - error
+                      lower_FUN <- suppressWarnings(FUN(object, R_lower, ...))
+                      lower_FUN <- ifelse(test = is.na(lower_FUN),
+                                          yes = Inf, no = lower_FUN)
+                      lower_error <- abs(lower_FUN - FUN_true)
+
+                      lower_FUN_acc <- c(lower_FUN_acc, lower_FUN)
+                      lower_error_acc <- c(lower_error_acc, lower_error)
+
+                      # Get & append the maximum error
+                      max_error <- max(lower_error, upper_error)
+                      max_error_acc <- c(max_error_acc, max_error)
+
+                  }
+
+                  return(data.frame(R = R, Error_R = R_error_acc,
+                                    FUN_true = FUN_acc,
+                                    upper_error = upper_error_acc,
+                                    lower_error = lower_error_acc,
+                                    max_abs_error = max_error_acc))
+              })
+
 
 #' Plot the fraction of objects in the max state (generic)
 #'
@@ -187,7 +310,6 @@ setMethod("getError", "Sensor", definition =
 #'
 #' @export
 #' @docType methods
-#' @rdname plotFractionMax-generic
 setGeneric('plotFractionMax', def = function(object, ...) standardGeneric("plotFractionMax"))
 
 #' Plot the fraction of sensors in the max state
@@ -249,7 +371,6 @@ setMethod("plotFractionMax", "redoxSensor", definition =
 #'
 #' @export
 #' @docType methods
-#' @rdname plotE-generic
 setGeneric('plotE', def = function(object) standardGeneric("plotE"))
 
 #' Plot the fraction of E of a redoxSensor
@@ -273,6 +394,46 @@ setMethod("plotE", "redoxSensor", definition =
                       geom_line(aes(x = R_E$R, y = R_E$E)) +
                       xlab("R")  +
                       ylab("E_GSH (mV)")
+
+                  return(plot)
+              })
+
+#' Plot the fraction of derivative of E of an object
+#'
+#' @param object An object
+#'
+#' @return A ggplot object
+#'
+#' @export
+#' @docType methods
+setGeneric('plotE_deriv', def = function(object) standardGeneric("plotE_deriv"))
+
+#' Plot the derivative of E of a redoxSensor
+#'
+#' Creates a ggplot object that has the ratio R on the horizontal axis
+#' and the derivative of redox potential (dE/dR) on the vertical axis
+#'
+#' @param object An redoxSensor object
+#'
+#' @return A ggplot object
+#'
+#' @export
+#' @docType methods
+#' @rdname plotE_deriv-redoxSensor
+setMethod("plotE_deriv", "redoxSensor", definition =
+              function(object) {
+                  R <- getR(object)
+
+                  # Cut the first and last values of R, since those
+                  # have an undefined derivitive
+                  R <- R[2:length(R)-1]
+
+                  R_E <- data.frame(R = R, E = getE_deriv(object, R))
+
+                  plot <- ggplot(R_E) +
+                      geom_line(aes(x = R_E$R, y = R_E$E)) +
+                      xlab("R")  +
+                      ylab("dE/dR (mV)")
 
                   return(plot)
               })
