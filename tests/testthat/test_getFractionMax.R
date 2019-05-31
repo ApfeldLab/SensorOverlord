4e03969d964bae7be39d0d8f515aa4802fbbc407
julianstanley <stanley.ju@husky.neu.edu>
2019-05-31 12:37:44 -0400

    [Test] Basic tests for getFractionMax

---
+++ tests/testthat/test_getFractionMax.R
@@ -0,0 +1,36 @@
+initSensor <- function() {
+    return(new("Sensor", Rmin = 1, Rmax = 5, delta = 0.5))
+}
+
+test_that("Fraction max on a regular sensor ranges between 0 and 1", {
+    sensor <- initSensor()
+    fractionMax <- getFractionMax(sensor)
+
+    expect_equal(fractionMax[1], 0)
+    expect_equal(fractionMax[length(fractionMax)], 1)
+})
+
+test_that("Fraction max works for all sensor types", {
+    sensor <- initSensor()
+    max_sensor <- getFractionMax(sensor)
+    max_redox <- getFractionMax(new("redoxSensor", sensor, e0 = -265))
+    max_pH <- getFractionMax(new("pHSensor", sensor, pKa = 7))
+
+    expect_equal(max_sensor, max_redox)
+    expect_equal(max_sensor, max_pH)
+})
+
+test_that("If delta is 1, middle fraction max should be 0.5/50%, but
+          not otherwise", {
+    sensor <- initSensor()
+
+    # Test with a delta of 0.5
+    max_sensor <- getFractionMax(sensor)
+    expect_false(median(max_sensor) == 0.5)
+
+    # Test with a delta of 1
+    sensor@delta = 1
+    max_sensor <- getFractionMax(sensor)
+
+    expect_equal(median(max_sensor), 0.5)
+})
