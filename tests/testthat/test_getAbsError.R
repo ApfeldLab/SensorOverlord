008a9f8bad54f7cc63519fb047215b671eb38b79
julianstanley <stanley.ju@husky.neu.edu>
2019-06-03 10:41:30 -0400

    [Fix] Added tests to catch length problems, [Feature] Added error randomization

--- tests/testthat/test_getAbsError.R
+++ tests/testthat/test_getAbsError.R
@@ -4,11 +4,19 @@ initSensor <- function() {
                delta = runif(1, min = 0.01, max = 4)))
 }
 
+initErrorModel <- function() {
+    Error <- runif(1, min = 0.001, max = 0.5)
+    function(x) {
+        x * Error
+    }
+}
+
+
 test_that("You can get errors from any sensor type", {
     sensor <- initSensor()
     redoxSensor <- new("redoxSensor", sensor, e0 = -265)
     pHSensor <- new("pHSensor", sensor, pKa = 7)
-    error_model <- function(x){0.02}
+    error_model <- initErrorModel()
 
     expect_type(getAbsError(sensor, FUN = getProperty,
                               Error_Model = error_model),
@@ -21,9 +29,17 @@ test_that("You can get errors from any sensor type", {
                 "double")
 })
 
+test_that("You get the same number of errors as Rs", {
+    sensor <- initSensor()
+    Error_Model <- initErrorModel()
+
+    expect_equal(length(getR(sensor)), length(getAbsError(sensor,
+                                                          Error_Model = Error_Model)))
+})
+
 test_that("You errors are positive", {
     sensor <- initSensor()
-    error_model <- function(x){0.02}
+    error_model <- initErrorModel()
 
     errors <- getAbsError(sensor, FUN = getProperty,
                           Error_Model = error_model)
