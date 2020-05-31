11f74d8a346a5712578906938739c1c030b8eb6a
julianstanley <stanley.ju@husky.neu.edu>
2020-05-30 20:34:41 -0400

    Added a utils function to get an error for multiple sensors

--- R/utils.R
+++ R/utils.R
@@ -10,31 +10,31 @@
 #' @rdname reScaleToRange-function
 #' @import stats
 rescaleToRange <- function(new_xs, old_xs, y) {
-    # Initalize rescaled y values
-    new_y <- c()
+  # Initalize rescaled y values
+  new_y <- c()
 
-    # Loop through each new x value
-    for (new_x in new_xs) {
-        # Initalize the closest old_x value found
-        closest_x_difference <- Inf
+  # Loop through each new x value
+  for (new_x in new_xs) {
+    # Initalize the closest old_x value found
+    closest_x_difference <- Inf
 
-        # Initalize the index corresponding to the closest old_x value found
-        closest_x_index <- NaN
+    # Initalize the index corresponding to the closest old_x value found
+    closest_x_index <- NaN
 
-        # Loop through each index of old x
-        for (old_x_index in 1:length(old_xs)) {
-            new_old_diff <- abs(old_xs[old_x_index] - new_x)
-            if (new_old_diff < closest_x_difference) {
-                closest_x_difference <- new_old_diff
-                closest_x_index <- old_x_index
-            }
-        }
-
-        # Accumulate new y values corresponding to the closest old_x found
-        new_y <- c(new_y, y[closest_x_index])
+    # Loop through each index of old x
+    for (old_x_index in 1:length(old_xs)) {
+      new_old_diff <- abs(old_xs[old_x_index] - new_x)
+      if (new_old_diff < closest_x_difference) {
+        closest_x_difference <- new_old_diff
+        closest_x_index <- old_x_index
+      }
     }
 
-    return(new_y)
+    # Accumulate new y values corresponding to the closest old_x found
+    new_y <- c(new_y, y[closest_x_index])
+  }
+
+  return(new_y)
 }
 
 
@@ -61,97 +61,97 @@ rescaleToRange <- function(new_xs, old_xs, y) {
 #'
 #' @export
 formatSpectraData <-
-    function(name,
-             type,
-             readout,
-             lambda_max,
-             values_max,
-             lambda_min,
-             values_min,
-             sensor_midpoint) {
-        # Data validation -------
+  function(name,
+           type,
+           readout,
+           lambda_max,
+           values_max,
+           lambda_min,
+           values_min,
+           sensor_midpoint) {
+    # Data validation -------
 
-        # Validating lengths
-        if (length(name) != 1) {
-            stop("Length of the name argument is not 1")
-        }
-
-        if (length(type) != 1) {
-            stop("Length of the type argument is not 1")
-        }
-
-        if (length(readout) != 1) {
-            stop("Length of the readout argument is not 1")
-        }
-
-        if (length(sensor_midpoint) != 1) {
-            stop("Length of the sensor_midpoint argument is not 1")
-        }
-
-
-        if (length(lambda_max) != length(values_max)) {
-            stop("The lambda and values for the maximum state have
-             different lengths")
-        }
-
-        if (length(lambda_min) != length(values_min)) {
-            stop("The lambda and values for the minimum state have
-             different lengths")
-        }
-
-        # Validating types
-        supported_types <- c("redox", "pH", "ATP")
-        supported_readouts <-
-            c("excitation ratiometric", "emission ratiometric")
-
-        if (typeof(name) != "character") {
-            stop("The name argument must be a character type")
-        }
-
-        if (typeof(type) != "character") {
-            stop("The type argument must be a character type")
-        }
-
-        if (typeof(readout) != "character") {
-            stop("The readout argument must be a character type")
-        }
-
-        if (!is.numeric(lambda_max)) {
-            stop("lambda_max must be numeric")
-        }
-
-        if (!is.numeric(values_max)) {
-            stop("values_max must be numeric")
-        }
-
-        if (!is.numeric(lambda_min)) {
-            stop("lambda_min must be numeric")
-        }
-
-        if (!is.numeric(values_min)) {
-            stop("values_min must be numeric")
-        }
-
-        if (!is.numeric(sensor_midpoint)) {
-            stop("sensor_midpoint must be numeric")
-        }
-
-        # ---------
-
-        # Create the final list
-        return(
-            list(
-                sensor_name = name,
-                sensor_type = type,
-                sensor_readout = readout,
-                lambda_max = lambda_max,
-                values_max = values_max,
-                lambda_min = lambda_min,
-                values_min = values_min,
-                sensor_midpoint = sensor_midpoint
-            )
-        )
+    # Validating lengths
+    if (length(name) != 1) {
+      stop("Length of the name argument is not 1")
     }
+
+    if (length(type) != 1) {
+      stop("Length of the type argument is not 1")
+    }
+
+    if (length(readout) != 1) {
+      stop("Length of the readout argument is not 1")
+    }
+
+    if (length(sensor_midpoint) != 1) {
+      stop("Length of the sensor_midpoint argument is not 1")
+    }
+
+
+    if (length(lambda_max) != length(values_max)) {
+      stop("The lambda and values for the maximum state have
+             different lengths")
+    }
+
+    if (length(lambda_min) != length(values_min)) {
+      stop("The lambda and values for the minimum state have
+             different lengths")
+    }
+
+    # Validating types
+    supported_types <- c("redox", "pH", "ATP")
+    supported_readouts <-
+      c("excitation ratiometric", "emission ratiometric")
+
+    if (typeof(name) != "character") {
+      stop("The name argument must be a character type")
+    }
+
+    if (typeof(type) != "character") {
+      stop("The type argument must be a character type")
+    }
+
+    if (typeof(readout) != "character") {
+      stop("The readout argument must be a character type")
+    }
+
+    if (!is.numeric(lambda_max)) {
+      stop("lambda_max must be numeric")
+    }
+
+    if (!is.numeric(values_max)) {
+      stop("values_max must be numeric")
+    }
+
+    if (!is.numeric(lambda_min)) {
+      stop("lambda_min must be numeric")
+    }
+
+    if (!is.numeric(values_min)) {
+      stop("values_min must be numeric")
+    }
+
+    if (!is.numeric(sensor_midpoint)) {
+      stop("sensor_midpoint must be numeric")
+    }
+
+    # ---------
+
+    # Create the final list
+    return(
+      list(
+        sensor_name = name,
+        sensor_type = type,
+        sensor_readout = readout,
+        lambda_max = lambda_max,
+        values_max = values_max,
+        lambda_min = lambda_min,
+        values_min = values_min,
+        sensor_midpoint = sensor_midpoint
+      )
+    )
+  }
 
 #' Returns the sensor database
 #'
@@ -160,27 +160,27 @@ formatSpectraData <-
 #' @export
 #' @import mongolite
 getDb <- function() {
-    options(
-        mongodb = list(
-            "host" = "sensoroverlordcluster-mnopd.mongodb.net",
-            "username" = "sensoroverlord",
-            "password" = "test"
-        )
+  options(
+    mongodb = list(
+      "host" = "sensoroverlordcluster-mnopd.mongodb.net",
+      "username" = "sensoroverlord",
+      "password" = "test"
     )
+  )
 
-    databaseName <- "sensordb"
-    collectionName <- "responses"
+  databaseName <- "sensordb"
+  collectionName <- "responses"
 
-    return(mongo(
-        collection = collectionName,
-        url = sprintf(
-            "mongodb+srv://%s:%s@%s/%s",
-            options()$mongodb$username,
-            options()$mongodb$password,
-            options()$mongodb$host,
-            databaseName
-        )
-    ))
+  return(mongo(
+    collection = collectionName,
+    url = sprintf(
+      "mongodb+srv://%s:%s@%s/%s",
+      options()$mongodb$username,
+      options()$mongodb$password,
+      options()$mongodb$host,
+      databaseName
+    )
+  ))
 }
 
 #' Adjusts a spectra, assuming that the actual spectra is not limiting
@@ -196,24 +196,24 @@ getDb <- function() {
 #' @export
 #' @import mongolite
 adjustSpectra <- function(spectra, fractionMax, fractionMin) {
-    adjusted_minimum <- ((spectra@values_maximum / fractionMax) -
-                             (spectra@values_minimum / fractionMin)) /
-        (((1 - fractionMax) / fractionMax) -
-             ((1 - fractionMin) / fractionMin))
+  adjusted_minimum <- ((spectra@values_maximum / fractionMax) -
+    (spectra@values_minimum / fractionMin)) /
+    (((1 - fractionMax) / fractionMax) -
+      ((1 - fractionMin) / fractionMin))
 
-    adjusted_maximum <- ((spectra@values_maximum / (1 - fractionMax)) -
-                             (spectra@values_minimum / (1 - fractionMin))) /
-        ((fractionMax / (1 - fractionMax)) -
-             ((fractionMin / (1 - fractionMin))))
+  adjusted_maximum <- ((spectra@values_maximum / (1 - fractionMax)) -
+    (spectra@values_minimum / (1 - fractionMin))) /
+    ((fractionMax / (1 - fractionMax)) -
+      ((fractionMin / (1 - fractionMin))))
 
-    return(
-        new(
-            "sensorSpectra",
-            lambdas = spectra@lambdas,
-            values_minimum = adjusted_minimum,
-            values_maximum = adjusted_maximum
-        )
+  return(
+    new(
+      "sensorSpectra",
+      lambdas = spectra@lambdas,
+      values_minimum = adjusted_minimum,
+      values_maximum = adjusted_maximum
     )
+  )
 }
 
 #' What is the fraction deprotenated of a certain pH, given the pKa?
@@ -225,8 +225,8 @@ adjustSpectra <- function(spectra, fractionMax, fractionMin) {
 #'
 #' @export
 fraction_deprot <- function(pH, pKa) {
-    return((10 ^ (pH - pKa)) /
-               (1 + 10 ^ (pH - pKa)))
+  return((10^(pH - pKa)) /
+    (1 + 10^(pH - pKa)))
 }
 
 
@@ -245,9 +245,8 @@ fraction_deprot <- function(pH, pKa) {
 #' E(R = 3, Rmin = 1, Rmax = 5, delta = 0.2, e0 = -275)
 #' @export
 E <- function(R, Rmin, Rmax, delta, e0, temp = 295.15) {
-    return(e0 - (8.315 * temp) / (2 * 96.48104) *
-               log( (delta * (Rmax - R)) / (R - Rmin))
-    )
+  return(e0 - (8.315 * temp) / (2 * 96.48104) *
+    log((delta * (Rmax - R)) / (R - Rmin)))
 }
 
 #' What was the recorded fluorescence ratio at a certain redox potential?
@@ -263,20 +262,21 @@ E <- function(R, Rmin, Rmax, delta, e0, temp = 295.15) {
 #' R_of_E(E = -254.5305, Rmin = 1, Rmax = 5, delta = 0.2, e0 = -275)
 #'
 #' # This is self-consistent with the E() function.
-#' R_of_E(E = E(R = 3, Rmin = 1, Rmax = 5, delta = 0.2, e0 = -275),
-#' Rmin = 1, Rmax = 5, delta = 0.2, e0 = -275)
+#' R_of_E(
+#'   E = E(R = 3, Rmin = 1, Rmax = 5, delta = 0.2, e0 = -275),
+#'   Rmin = 1, Rmax = 5, delta = 0.2, e0 = -275
+#' )
 #' @export
 R_of_E <- function(E, Rmin, Rmax, delta, e0, temp = 295.15) {
-    return(
-
-        (
-            Rmin * exp(((e0 - E) * 2 * 96.48104)/(8.315*temp)) + delta*Rmax
-        )
-        /
-            (
-                exp(((e0 - E) * 2 * 96.48104)/(8.315*temp)) + delta
-            )
+  return(
+    (
+      Rmin * exp(((e0 - E) * 2 * 96.48104) / (8.315 * temp)) + delta * Rmax
     )
+    /
+      (
+        exp(((e0 - E) * 2 * 96.48104) / (8.315 * temp)) + delta
+      )
+  )
 }
 
 #' What is the error in redox potential at a given redox potential (mV),
@@ -295,18 +295,20 @@ R_of_E <- function(E, Rmin, Rmax, delta, e0, temp = 295.15) {
 #' (4) 'max_error' the largest possible difference between the observed and expected E
 #'
 #' @examples
-#' Error_E(E = -275, Rmin = 1, Rmax = 5, delta = 0.2, e0 = -275, error_R = function(x) 0.02*x)
+#' Error_E(E = -275, Rmin = 1, Rmax = 5, delta = 0.2, e0 = -275, error_R = function(x) 0.02 * x)
 #' @export
 Error_E <- function(E, Rmin, Rmax, delta, e0, error_R, temp = 295.15) {
-    R <- R_of_E(E, Rmin, Rmax, delta, e0, temp)
-    larger_E <- suppressWarnings(E(R + error_R(R), Rmin, Rmax, delta, e0, temp))
-    larger_E[is.nan(larger_E)] <- Inf
-    smaller_E <- suppressWarnings(E(R - error_R(R), Rmin, Rmax, delta, e0, temp))
-    smaller_E[is.nan(smaller_E)] <- Inf
-    return(list(E = E,
-                larger_E = larger_E,
-                smaller_E = smaller_E,
-                max_error = pmax(abs(E - larger_E), abs(E - smaller_E))))
+  R <- R_of_E(E, Rmin, Rmax, delta, e0, temp)
+  larger_E <- suppressWarnings(E(R + error_R(R), Rmin, Rmax, delta, e0, temp))
+  larger_E[is.nan(larger_E)] <- Inf
+  smaller_E <- suppressWarnings(E(R - error_R(R), Rmin, Rmax, delta, e0, temp))
+  smaller_E[is.nan(smaller_E)] <- Inf
+  return(list(
+    E = E,
+    larger_E = larger_E,
+    smaller_E = smaller_E,
+    max_error = pmax(abs(E - larger_E), abs(E - smaller_E))
+  ))
 }
 
 #' Creates a dataframe of errors at given inaccuracies
@@ -328,16 +330,80 @@ Error_E <- function(E, Rmin, Rmax, delta, e0, error_R, temp = 295.15) {
 #' create_error_df_redox(c(0.01, 0.02), -300, -200, 1, 5, 0.2, -275)
 #' @export
 create_error_df_redox <- function(inaccuracies, Emin, Emax, Rmin, Rmax, delta, e0, temp = -295.15, by = 0.01) {
-    error_df_full <- data.frame(E = c(), Error = c(), Inaccuracy = c())
+  error_df_full <- data.frame(E = c(), Error = c(), Inaccuracy = c())
+  for (inaccuracy in inaccuracies) {
+    error <- Error_E(
+      E = seq(Emin, Emax, by = by),
+      Rmin = Rmin, Rmax = Rmax, delta = delta, e0 = e0,
+      error_R = function(x) inaccuracy * x, temp = temp
+    )
+    error_df_full <- rbind(
+      error_df_full,
+      data.frame(
+        E = error$E,
+        Error = error$max_error,
+        Inaccuracy = as.character(rep(inaccuracy, length(error$E)))
+      )
+    )
+  }
+  error_df_full
+}
+
+#' @param inaccuracies A vector of inaccuracies (e.g. 0.02 for 2\% error), always relative
+#' @param Emin The minimum redox potential, in mV, for which to record error
+#' @param Emax The maximum redox potential, in mV, for which to record error
+#' @param param_df A dataframe containing a list of sensor parameters, with these columns:
+#' 'name': An identifier for this sensor
+#' 'Rmin': the minimum possible ratiometric fluorescence for this sensor
+#' 'Rmax': the maximum possible ratiometric fluorescence for this sensor
+#' 'delta': the ratiometric fluorescence in the first wavelength for this sensor
+#' 'e0': this sensor's midpoint potential
+#' @param temp (optional, default: 295.15) the temperature (in Kelvin) at which measurements were made
+#' @param by (optional, default: 0.01) The granularity of the error table--e.g., by = 0.01 would record 275 and 275.01, etc.
+#' @return A dataframe of errors with columns:
+#' 'E': the redox potential (mV),
+#' 'Rmin': the minimum possible ratiometric fluorescence for this sensor
+#' 'Rmax': the maximum possible ratiometric fluorescence for this sensor
+#' 'Error': the error in this redox potential (mV)
+#' 'Inaccuracy': The inaccuracy of the measurements (relative to R).
+#' Creates an error df at multiple inaccuracies, with multiple Rmin/Rmax/delta/e0 parameters
+#' @examples
+#' create_error_df_redox_multiple(
+#' c(0.01, 0.02), -300, -200,
+#' data.frame(
+#'     "Rmin" = c(1, 2),
+#'     "Rmax" = c(5, 6),
+#'     "delta" = c(0.2, 1.2),
+#'     "name" = c("normal", "plusOne"),
+#'     "e0" = c(-275, -274)
+#' )
+#' )
+#' @export
+create_error_df_redox_multiple <- function(inaccuracies, Emin, Emax, param_df, temp = -295.15, by = 0.01) {
+  error_df_full <- data.frame(E = c(), Rmin = c(), Rmax = c(),
+                              Name = c(), Error = c(), Inaccuracy = c())
+  # Loop through each sensor in the param_df
+  for(n in 1:nrow(param_df)) {
+    sensor_params <- param_df[n, ]
     for(inaccuracy in inaccuracies) {
-        error <- Error_E(E = seq(Emin, Emax, by = by),
-                         Rmin = Rmin, Rmax = Rmax, delta = delta, e0 = e0,
-                         error_R = function(x) inaccuracy*x, temp = temp)
-        error_df_full <- rbind(error_df_full,
-                               data.frame(E = error$E,
-                                          Error = error$max_error,
-                                          Inaccuracy = as.character(rep(inaccuracy, length(error$E))))
+        error <- Error_E(
+            E = seq(Emin, Emax, by = by),
+            Rmin = sensor_params[,"Rmin"], Rmax = sensor_params[,"Rmax"],
+            delta = sensor_params[,"delta"], e0 = sensor_params[,"e0"],
+            error_R = function(x) inaccuracy * x, temp = temp
+        )
+        error_df_full <- rbind(
+            error_df_full,
+            data.frame(
+                E = error$E,
+                Rmin = as.character(rep(sensor_params[,"Rmin"], length(error$E))),
+                Rmax = as.character(rep(sensor_params[,"Rmax"], length(error$E))),
+                Name = as.character(rep(sensor_params[,"name"], length(error$E))),
+                Error = error$max_error,
+                Inaccuracy = as.character(rep(inaccuracy, length(error$E)))
+            )
         )
     }
-    error_df_full
+  }
+  error_df_full
 }
