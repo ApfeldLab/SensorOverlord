c21883bc13003d0ba1a8cbb140065ecf1c7fec2a
julianstanley <stanley.ju@husky.neu.edu>
2020-08-08 09:54:37 -0400

    Ensure that newSensorFromSpectra recieves a range of lambda values; accept all Rmin and Rmax

--- R/Sensor_Constructors.R
+++ R/Sensor_Constructors.R
@@ -22,6 +22,11 @@ newSensorFromSpectra <-
         values_minimum = sensorSpectra@values_minimum
         lambdas = sensorSpectra@lambdas
 
+        if(length(lambda_1) < 2 | length(lambda_2) < 2) {
+            stop("Lambda 1 and lambda 2 should be vectors, not scalars.
+                 Please enter two values describing the range of possible lambda values.")
+        }
+
         # Make boolean vectors describing which of the lambdas are relevant
         relevant_lambda_1 <-
             (lambdas >= lambda_1[1] & lambdas <= lambda_1[2])
@@ -38,6 +43,13 @@ newSensorFromSpectra <-
         Rmax <- mean(values_maximum[relevant_lambda_1]) /
             mean(values_maximum[relevant_lambda_2])
 
+        # If Rmin is bigger than Rmax, switch them
+        if(Rmin > Rmax) {
+            Rmin_new <- Rmax
+            Rmax_new <- Rmin
+            Rmin <- Rmin_new
+            Rmax <- Rmax_new
+        }
         # Make a new sensor
         return(new(
             "Sensor",
